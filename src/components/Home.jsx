e8f4e2cd16299bc443e9e5d656e79c0729247dac
Davis Kim <dkim0509@gmail.com>
2024-08-07 16:13:07 -0400

    added math fact functionality and answer box

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+
+const Home = () => {
+  return (
+    //contains links (navbar?) to the other components.
+    //navbar will contain inventory, map, notebook, playerstats (the players icon will lead them to the playerstats page), and settings
+    <div>HOME!</div>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+
+const Navbar = () => {
+  return (
+    //contains links (navbar?) to the other components.
+    //navbar will contain inventory, map, notebook, playerstats (the players icon will lead them to the playerstats page), and settings
+    <div>Navbar</div>
+  );
+};
+
+export default Navbar;
